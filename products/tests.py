d3d6f095eef21863518201fc4d04d369b213d873
Danilo11 <danilo11franca@yahoo.com>
2021-05-31 05:18:42 -0300

    arquivo de testes unitários

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 # Create your tests here.
+class YourTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', password='example')
+        pass
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/api/products')
+        self.assertEqual(response.status_code, 200)
